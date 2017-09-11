84d0771e095240b0a4e876d68810f7c174e14a6f
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-11 16:41:02 -0500

    Update configure_hybris.sh

--- scripts/configure_hybris.sh
+++ scripts/configure_hybris.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # Copy local.properties file for External MySQL connectivity
-
 /bin/cp /var/www/codedeploy/hybris/local.properties /home/hybris/hybris/bin/conf
 chown -R hybris /home/hybris/hybris
 
 # Copy Service file
-
 /bin/cp /var/www/codedeploy/hybris/hybris.service /usr/lib/systemd/system
 
 systemctl daemon-reload
+
+#Add Firewall Rule for External Access
+setenforce 0
+systemctl start firewalld
+systemctl enable firewalld
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=9001/tcp --permanent
+firewall-cmd --zone=public --add-port=9002/tcp --permanent
+firewall-cmd --reload
