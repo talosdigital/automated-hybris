a57b9ed6763c6343fdb6fde56f227fec8223ebd3
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-11 16:44:46 -0500

    Update configure_hybris.sh

--- scripts/configure_hybris.sh
+++ scripts/configure_hybris.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Copy local.properties file for External MySQL connectivity
-/bin/cp /var/www/codedeploy/hybris/local.properties /home/hybris/hybris/bin/conf
+/bin/cp /opt/codedeploy/hybris/local.properties /home/hybris/hybris/bin/conf
 chown -R hybris /home/hybris/hybris
 
 # Copy Service file
-/bin/cp /var/www/codedeploy/hybris/hybris.service /usr/lib/systemd/system
+/bin/cp /opt/codedeploy/hybris/hybris.service /usr/lib/systemd/system
 
 systemctl daemon-reload
 
