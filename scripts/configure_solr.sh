ff6ca61efb33ea1f288d1a102ae1059f3fb3dfb7
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-13 16:48:30 -0500

    Update configure_solr.sh

--- scripts/configure_solr.sh
+++ scripts/configure_solr.sh
@@ -7,3 +7,19 @@ wget http://apache.mirrors.lucidnetworks.net/lucene/solr/6.6.1/solr-6.6.1-src.tg
 tar -xvf solr-6.6.1-src.tgz
 mv /opt/solr-6.6.1-src /opt/solr
 mv /opt/solr/example /opt/solr/core
+
+# Copy Service file
+/bin/cp /opt/codedeploy/solr/solr /etc/init.d
+chmod +x /etc/init.d/solr
+
+chkconfig --add solr
+
+systemctl daemon-reload
+
+#Add Firewall Rule for External Access
+setenforce 0
+systemctl start firewalld
+systemctl enable firewalld
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+firewall-cmd --zone=public --add-port=8983/tcp --permanent
+firewall-cmd --reload
