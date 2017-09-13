214e249153f9d973dcb26870b689dba37f48eb4b
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-13 17:18:10 -0500

    Update configure_solr.sh

--- scripts/configure_solr.sh
+++ scripts/configure_solr.sh
@@ -5,7 +5,7 @@
 cd /opt
 wget http://apache.mirrors.lucidnetworks.net/lucene/solr/6.6.1/solr-6.6.1-src.tgz
 tar -xvf solr-6.6.1-src.tgz
-mv /opt/solr-6.6.1-src /opt/solr
+mv /opt/solr-6.6.1 /opt/solr
 mv /opt/solr/example /opt/solr/core
 
 # Copy Service file
