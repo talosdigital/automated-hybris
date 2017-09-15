d03da235efba7a6061682490f84ea8622f55a4e2
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-15 14:17:31 -0500

    Update build_env.sh

--- scripts/build_env.sh
+++ scripts/build_env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Set and Build the Environment
+# Link config folder to EFS
+ln -sfn /home/hybris/hybris/config/ /mnt/efs/hybris/
 
+# Set and Build the Environment
 cd /home/hybris/hybris/bin/platform
 . ./setantenv.sh
 ant clean 
