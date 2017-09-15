918222a98b5276c32e8e405704a26f614d62bdff
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-15 09:46:13 -0500

    Create mount_efs.sh

---
+++ scripts/mount_efs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Create folder and mount EFS volume
+mkdir -p /mnt/efs
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-f449dcbd.efs.us-east-1.amazonaws.com:/ /mnt/efs
+
+#Automount EFS Volume
+fstab=/etc/fstab
+if [[ $(grep -q "fs-f449dcbd.efs.us-east-1.amazonaws.com" "$fstab") ]]
+then
+    echo "fs-f449dcbd.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0" >> /etc/fstab
+else
+    echo "Entry in fstab exists."
+fi
+
+mkdir -p /mnt/efs/hybris/custom
+mkdir -p /mnt/efs/hybris/config
