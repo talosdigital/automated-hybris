c870906d9991cd0de707d6c769d221141713d2ee
Pablo del Pino <pdelpino@delpinoconsulting.com>
2017-09-20 12:02:36 -0500

    Update build_env.sh

--- scripts/build_env.sh
+++ scripts/build_env.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # Link config folder to EFS
-ln -sfn /home/hybris/hybris/config/ /mnt/efs/hybris/
+#ln -sfn /home/hybris/hybris/config/ /mnt/efs/hybris/
 
 # Set and Build the Environment
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "TD-Hybris-HAC" ]
+then
 cd /home/hybris/hybris/bin/platform
 . ./setantenv.sh
 ant clean 
 ant initialize
 ant all
+fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "TD-Hybris-FE" ]
+then
+cd /home/hybris/installer
+./install.sh -r b2c_acc initialize
+fi
+
